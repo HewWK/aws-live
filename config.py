c7b68a5f1fe0da66a6e1779280627c487316e52d
HewWK <143489299+HewWK@users.noreply.github.com>
2023-08-29 15:40:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.coh4awj521gn.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "hewweiken-employee"
+customregion = "us-east-1"
+
